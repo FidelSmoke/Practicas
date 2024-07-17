f2f4d11a7755bc1384d06efd52b36fbec937f92f
FidelSmoke <146258944+FidelSmoke@users.noreply.github.com>
2024-07-17 13:45:20 -0500

    listop

--- dashboard-react/dashboard-react/src/Components/Navbar.js
+++ dashboard-react/dashboard-react/src/Components/Navbar.js
@@ -2,7 +2,7 @@ import 'bootstrap/scss/bootstrap.scss'
 import 'bootstrap/dist/css/bootstrap.min.css';
 const Navbar = () => {
     return (
-        <div className="navbar border-bottom mt-100" >
+        <div className="navbar border-bottom mt-100 bg" >
              <small className='text-red  d-sm-block fw-bold '>Hola, Administrador</small>
         </div>
     )
