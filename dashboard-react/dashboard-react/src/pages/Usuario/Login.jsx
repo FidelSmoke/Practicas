dc8283debe5dae0bf0d396a68ef76af9dcd6a6e1
FidelSmoke <146258944+FidelSmoke@users.noreply.github.com>
2024-08-06 10:26:24 -0500

    kkkkkk

--- dashboard-react/dashboard-react/src/pages/Usuario/Login.jsx
+++ dashboard-react/dashboard-react/src/pages/Usuario/Login.jsx
@@ -2,8 +2,12 @@ import React from 'react'
 
 export default function Login() {
     return (
-        <div>
-            <h3 className='text-white'>ola</h3>
+        <div className='container'>
+            <h1 className='text-white text-center mx-5'><i class="bi bi-4-circle-fill mx-3 text-info"></i>LOGIN</h1>
+            <div class="input-group mb-3">
+                <button class="btn btn-outline-secondary" type="button" id="button-addon1">Button</button>
+                <input type="text" class="form-control" placeholder="" aria-label="Example text with button addon" aria-describedby="button-addon1" />
+            </div>
         </div>
     )
 }
