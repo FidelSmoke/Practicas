f2a361bcf22cdadd1806751281c617a1ba39cf99
DavidV4iss <163668311+DavidV4iss@users.noreply.github.com>
2024-08-06 12:49:13 -0500

    sa
    sa

--- dashboard-react/dashboard-react/src/pages/Usuario/Login.jsx
+++ dashboard-react/dashboard-react/src/pages/Usuario/Login.jsx
@@ -2,11 +2,25 @@ import React from 'react'
 
 export default function Login() {
     return (
-        <div className='container'>
-            <h1 className='text-white text-center mx-5'><i class="bi bi-4-circle-fill mx-3 text-info"></i>LOGIN</h1>
-            <div class="input-group mb-3">
-                <button class="btn btn-outline-secondary" type="button" id="button-addon1">Button</button>
-                <input type="text" class="form-control" placeholder="" aria-label="Example text with button addon" aria-describedby="button-addon1" />
+        
+        <div className="min-vh-100 p-5 align-content-center ">
+            <div className='container p-5 w-50 border border- bg-white table-responsive border rounded '>
+                <h1 className='text-danger text-center anton mb-4'>¡Ingresa Ya!</h1>
+                <div class="input-group mb-3 ">
+                    <span class="input-group-text" id="basic-addon1"><i class="bi bi-person"></i></span>
+                    <input type="text" class="form-control" placeholder="Nombre de usuario" aria-label="Username" aria-describedby="basic-addon1" />
+                </div>
+                <div class="input-group mb-3">
+                    <span class="input-group-text" id="basic-addon2"><i class="bi bi-envelope"></i></span>
+                    <input type="text" class="form-control" placeholder="Correo electronico" aria-label="Recipient's username" aria-describedby="basic-addon2" />
+                </div>
+                <div class="input-group mb-3">
+                    <span class="input-group-text"><i class="bi bi-file-lock"></i></span>
+                    <input type="text" class="form-control" placeholder='Contraseña' aria-label="Amount (to the nearest dollar)" />
+                </div>
+                <div className=' text-center mt-4'>
+                    <button type="button" class="btn btn-danger .col-md-4" data-bs-toggle="modal" data-bs-target="#AñadirModal" data-bs-whatever="@mdo" >Continuar</button>
+                </div>
             </div>
         </div>
     )
