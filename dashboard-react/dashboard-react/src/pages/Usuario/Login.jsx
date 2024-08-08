27ac3798380ccb0f4c4169da59c4fa534968309d
DavidV4iss <163668311+DavidV4iss@users.noreply.github.com>
2024-08-08 18:13:26 -0500

    s
    s

--- dashboard-react/dashboard-react/src/pages/Usuario/Login.jsx
+++ dashboard-react/dashboard-react/src/pages/Usuario/Login.jsx
@@ -4,7 +4,7 @@ export default function Login() {
     return (
         <div class="login">
             <div className="min-vh-100 p-5 align-content-center mx-5 justify-content-end">
-                <div className='container p-5 w-50 border border- bg-white table-responsive border rounded '>
+                <div className='container p-5 w-50 border border bg-body-secondary table-responsive border rounded-4 '>
                     <div className="row">
                         <div className="col-6">
                             <img src="/LOGO.png" alt="" className='img-fluid' />
@@ -13,8 +13,8 @@ export default function Login() {
                             <h1 className='text-danger text-center anton mb-4'>¡Ingresa Ya!</h1>
 
                             <div class="text-center">
-                                <button class="btn btn  mb-3 border border rounded-circle" type="button"><i class="bi bi-google"></i></button>
-                                <button class="btn btn  mb-3 mx-3 border border rounded-circle" type="button"><i class="bi bi-facebook"></i></button>
+                                <button class="btn btn  mb-3 border border rounded-circle text-dark bg-white" type="button"> <i class="bi bi-google"></i> </button>
+                                <button class="btn btn  mb-3 mx-3 border border rounded-circle text-primary bg-white" type="button"><i class="bi bi-facebook"></i></button>
                             </div>
 
 
@@ -31,7 +31,7 @@ export default function Login() {
                                 <input type="text" class="form-control" placeholder='Contraseña' aria-label="Amount (to the nearest dollar)" />
                             </div>
 
-                            <p><a href="#" class="link-underline-light">¿Olvidaste tu contraseña?</a></p>
+                            <p><a href="#" class="link-underline-light text-danger">¿Olvidaste tu contraseña?</a></p>
                             <div className=' text-center mt-4'>
                                 <a href="/Index"><button type="button" class="btn btn-danger .col-md-4">Continuar</button></a>
                             </div>
