fae735c20b6fbfb09c0d1502bc8896dd969cca00
FidelSmoke <146258944+FidelSmoke@users.noreply.github.com>
2024-08-12 13:57:55 -0500

    ga

--- Mater Barber/Codigo React/src/App.jsx
+++ Mater Barber/Codigo React/src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Route, BrowserRouter, Routes } from 'react-router-dom'
 import AddBarberos from './pages/Admin/AddBarberos';
+import evaluacion from './pages/Usuario/evaluacion';
 import Inventario from './pages/Admin/inventario'
 import Perfil from './pages/Admin/Perfil';
 import Inicio from './pages/Admin/Inicio';
@@ -14,6 +15,7 @@ function App() {
         <div className='content w-100'>
           <Routes>
             <Route exact path='/' element={<Index />}></Route>
+            <Route exact path='/indexdos' element={<evaluacion />}></Route>
             <Route exact path='/Inventario' element={<Inventario />}> </Route>
             <Route exact path='/Addbarberos' element={<AddBarberos />}> </Route>
             <Route exact path='/Perfil' element={<Perfil />}> </Route>
