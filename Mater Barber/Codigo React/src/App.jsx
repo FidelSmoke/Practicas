e5721adc253c6281f1c4b3e18045af105d492be0
DavidV4iss <163668311+DavidV4iss@users.noreply.github.com>
2024-08-12 17:03:19 -0500

    ss
    s

--- Mater Barber/Codigo React/src/App.jsx
+++ Mater Barber/Codigo React/src/App.jsx
@@ -1,12 +1,10 @@
 import { BrowserRouter as Router, Route, BrowserRouter, Routes } from 'react-router-dom'
 import AddBarberos from './pages/Admin/AddBarberos';
-import evaluacion from './pages/Usuario/evaluacion';
 import Inventario from './pages/Admin/inventario'
 import Perfil from './pages/Admin/Perfil';
 import Inicio from './pages/Admin/Inicio';
 import Login from './pages/Usuario/Login'
 import Index from './pages/Usuario/Index';
-import Registro from './pages/Usuario/Registro';
 import './App.scss';
 import 'bootstrap/scss/bootstrap.scss'
 function App() {
@@ -15,9 +13,7 @@ function App() {
       <div className='flex'>
         <div className='content w-100'>
           <Routes>
-            <Route exact path='/Registro' element={<Registro />}></Route>
             <Route exact path='/' element={<Index />}></Route>
-            <Route exact path='/indexdos' element={<evaluacion />}></Route>
             <Route exact path='/Inventario' element={<Inventario />}> </Route>
             <Route exact path='/Addbarberos' element={<AddBarberos />}> </Route>
             <Route exact path='/Perfil' element={<Perfil />}> </Route>
--- Mater Barber/Codigo React/src/pages/Usuario/evaluacion.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function evaluacion() {
-    return (
-        <div>
-            <h1 className="text-white">hola</h1>
-        </div>
-    )
-}
