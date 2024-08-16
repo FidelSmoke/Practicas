419bbfdf48652bd87cf5927413a8cf9db0f2fdd5
FidelSmoke <146258944+FidelSmoke@users.noreply.github.com>
2024-08-15 22:14:22 -0500

    Registro Backend Corregir

--- Mater Barber/Backend/server.js
+++ Mater Barber/Backend/server.js
@@ -30,8 +30,48 @@ app.post('/login', (req, res) => {
             return res.send("No Registrado")
         }
     })
+});
+
+
+
+
+
+app.post('/Registro', (req, res) => {
+    const sql = "SELECT * FROM usuarios WHERE email = ?";
+
+    db.query(sql, [req.body.nombre_usuario,req.body.email, req.body.nit, req.body.telefono, req.body.password,req.body.confirmar_contraseña, req.body.id_rol], (err, data) => {
+
+        if (err) {
+            return res.status(500).send(err)
+        }
+
+        else if (result.length > 0) {
+            return res.status(400).send("Usuario Existente Intenta Iniciar Sesion")
+        }
+
+        else if (password !== confirmPassword) {
+            return res.status(400).send('Las contraseñas no coinciden');
+        }
+
+        else if (password.length < 8) {
+            return res.status(400).send('La contraseña debe tener al menos 8 caracteres');
+        }
+
+        else if (telefono !== telefono_usuario) {
+            return res.status(400).send('Este Numero Telefonico Ya Se Encuentra Registrado');
+        }
+
+        if (data.length > 0) {
+            return res.status(500).send("Registrado correctamente");
+        }
+        
+    })
 })
+
+
 
 app.listen(8081, () => {
     console.log("Conexion exitosa:)")
-})
+});
+
+
--- Mater Barber/Codigo React/src/pages/Usuario/Registro.jsx
+++ Mater Barber/Codigo React/src/pages/Usuario/Registro.jsx
@@ -1,53 +1,50 @@
 import 'bootstrap/scss/bootstrap.scss'
 import React from 'react'
-// import axios from 'axios'
-// import Swal from 'sweetalert2'
-// import useNavigate from 'react'
-// import useState from 'react'
+import axios from 'axios'
+import Swal from 'sweetalert2'
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+
+
 export default function Registro() {
 
-    // axios.defaults.withCredentials = true
+    axios.defaults.withCredentials = true
+    
+    const [nombre_usuario, setNombre_usuario] = useState('')
+    const [email, setEmail] = useState('')
+    const [nit, setNit] = useState('')
+    const [telefono, setTelefono] = useState('')
+    const [password, setPassword] = useState('')
+    // const [confirmar_contraseña, setConfirmar_contraseña] = useState('')
 
-    // const navigate = useNavigate();
 
-    // const [user, setUser] = useState({
-    //   nombre_usuario: "",
-    //   email: "",
-    //   nit:"",
-    //   telefono:"",
-    //   contraseña: "",
-    //   confirmar_contraseña: ""
+    const navigate = useNavigate()
 
-    // });
-
-    // const handleChange = (e) => {
-    //   setUser(prev => ({ ...prev, [e.target.name]: e.target.value }));
-    // }
-
-    // const handleSubmit = async (e) => {
-    //   e.preventDefault();
-    //   try {
-    //     const res = await axios.post("http://localhost:8080/registro", user);
-    //     if (res.status === 200) {
-    //       Swal.fire({
-    //         title: 'Cuenta creada',
-    //         text: 'Cuenta creada correctamente',
-    //         icon: 'success',
-    //         confirmButtonText: 'Continuar'
-    //       });
-    //       navigate("/Login");
-    //     }
-    //   } catch(error){
-    //     console.log(error);
-    //     if(error.response){
-    //       Swal.fire({
-    //         title: error.response.data || 'Credenciales incorrectas',
-    //         icon: 'error',
-    //         confirmButtonText: 'Intentar de nuevo'
-    //       });
-    //     }
-    //   }
-    // } 
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            axios.post("http://localhost:8081/Registro", { nombre_usuario, email, nit, telefono, password })
+                .then(res => {
+                    console.log(res)
+                    if (res.data === "Registro Exitoso") {
+                        Swal.fire({
+                            position: "top-end",
+                            icon: "success",
+                            title: "Te Has Registrado correctamente",
+                            showConfirmButton: false,
+                            timer: 1500
+                        });
+                        navigate("/Login")
+                    }
+                    else {
+                        Swal.fire("Error ", "Correo electronico o contraseña incorrectas!", "error");
+                    }
+                })
+                .catch(err => console.log(err))
+        } catch (err) {
+            console.log(err)
+        }
+    } 
     return (
         <div className="Registro">
             <div className="min-vh-100 p-5 align-content-center mx-5 justify-content-end">
@@ -59,54 +56,54 @@ export default function Registro() {
                         <div className="col-6">
                             <h1 className='text-white text-center anton mb-4'>¡Registrate Aqui!</h1>
 
-                            <form className='row g-1'> 
+                            <form className='row g-1' onSubmit={handleSubmit}> 
+                               
                                 
                                 <div className="text-center ">
                                     <button className="btn btn  mb-3 border border rounded-circle text-primary bg-white" type="button"> <i className="bi bi-google"></i> </button>
                                     <button className="btn btn  mb-3 mx-3 border border rounded-circle text-primary bg-white" type="button"><i className="bi bi-facebook"></i></button>
                                 </div> 
-
-                                <label for="validationCustom01" className="texto form-label text-white">Nombre de usuario</label>
+                                <label for="validationCustom01" className="texto form-label text-white" htmlFor="floatingInput" >Nombre de usuario</label>
                                 <div className="input-group mb-3 ">
                                     <span className="input-group-text" id="basic-addon2"><i className="bi bi-person"></i></span>
-                                    <input type="text" className="form-control" name='nombre_usuario' placeholder="Introduce tu nombre de usuario" aria-label="Recipient's username" aria-describedby="basic-addon2"
+                                    <input type="text" className="form-control" name='nombre_usuario' placeholder="Introduce tu nombre de usuario" aria-label="Recipient's username" aria-describedby="basic-addon2" onChange={e => setNombre_usuario(e.target.value)} 
                                     />
                                 </div>
 
                                 <label for="validationCustom01" className="texto form-label text-white">Correo electronico</label>
                                 <div className="input-group mb-3">
                                     <span className="input-group-text" id="basic-addon2"><i className="bi bi-envelope"></i></span>
-                                    <input type="email" className="form-control" name='email' placeholder="Ej: pepitoperez@gmail.com" aria-label="Recipient's username" aria-describedby="basic-addon2"
+                                    <input type="email" className="form-control" name='email' placeholder="Ej: pepitoperez@gmail.com" aria-label="Recipient's username" aria-describedby="basic-addon2" onChange={e => setEmail(e.target.value)}
                                     />
                                 </div>
 
                                 <label for="validationCustom01" className="texto form-label text-white">Numero de identificacion</label>
                                 <div className="input-group mb-3 ">
                                     <span className="input-group-text"><i className="bi bi-person-vcard"></i></span>
-                                    <input type="text" className="form-control" name='nit' placeholder='Introduce tu numero de identificacion' aria-label="Amount (to the nearest dollar)"
+                                    <input type="text" className="form-control" name='nit' placeholder='Introduce tu numero de identificacion' aria-label="Amount (to the nearest dollar)" onChange={e => setNit(e.target.value)}
                                     />
                                 </div>
 
                                 <label for="validationCustom01" className="texto form-label text-white">Telefono</label>
                                 <div className="input-group mb-3">
                                     <span className="input-group-text"><i className="bi bi-telephone-plus"></i></span>
-                                    <input type="text" className="form-control" name='telefono' placeholder='Introduce tu numero de celular' aria-label="Amount (to the nearest dollar)"
+                                    <input type="text" className="form-control" name='telefono' placeholder='Introduce tu numero de celular' aria-label="Amount (to the nearest dollar)" onChange={e => setTelefono(e.target.value)}
                                     />
                                 </div>
 
                                 <label for="validationCustom01" className="texto form-label text-white">Contraseña</label>
                                 <div className="input-group mb-3">
                                     <span className="input-group-text"><i className="bi bi-file-lock"></i></span>
-                                    <input type="password" className="form-control" name='contraseña' placeholder='Crea tu contraseña' aria-label="Amount (to the nearest dollar)"
+                                    <input type="password" className="form-control" name='contraseña' placeholder='Crea tu contraseña' aria-label="Amount (to the nearest dollar)" onChange={e => setPassword(e.target.value)}
                                     />
                                 </div>
 
-                                <label for="validationCustom01" className="texto form-label text-white">Confima tu contraseña</label>
+                                {/* <label for="validationCustom01" className="texto form-label text-white">Confima tu contraseña</label>
                                 <div className="input-group mb-3">
                                     <span className="input-group-text"><i className="bi bi-file-lock"></i></span>
-                                    <input type="password" className="form-control" name='confirmar_contraseña' placeholder='Confirma tu contraseña' aria-label="Amount (to the nearest dollar)"
+                                    <input type="password" className="form-control" name='confirmar_contraseña' placeholder='Confirma tu contraseña' aria-label="Amount (to the nearest dollar)" onChange={e => setConfirmar_contraseña(e.target.value)}
                                     />
-                                </div>
+                                </div> */}
 
                                 <div class="col-12">
                                     <div class="form-check">
