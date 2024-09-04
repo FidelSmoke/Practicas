f87b5861fb94c6ba00e8980a6c6884ce17500651
FidelSmoke <146258944+FidelSmoke@users.noreply.github.com>
2024-09-04 15:37:49 -0500

    Se Arreglo Navbar De Usuario

--- Mater Barber/Frontend/src/Components/NavbarUsuario.jsx
+++ Mater Barber/Frontend/src/Components/NavbarUsuario.jsx
@@ -10,7 +10,7 @@ export default function NavbarUsuario() {
                     <button class="navbar-toggler border border-white" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon "></span>
                     </button>
-                    <div class="offcanvas offcanvas-end text-bg-dark" tabindex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
+                    <div class="offcanvas offcanvas-end text-bg-dark bebas" tabindex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
                         <div class="offcanvas-header">
                             <h5 class="offcanvas-title text-warning" id="offcanvasDarkNavbarLabel">Menu De Navegación</h5>
                             <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
@@ -18,31 +18,31 @@ export default function NavbarUsuario() {
                         <div class="offcanvas-body">
                             <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
                                 <li class="nav-item">
-                                    <a class="nav-link active" aria-current="page" href="#home"><i class="bi bi-arrow-right mx-3"></i>Home</a>
+                                    <a class="nav-link active" aria-current="page" href="#home"><i class="bi bi-house-door-fill mx-3"></i>Home</a>
                                 </li>
                                 <li class="nav-item">
-                                    <a class="nav-link active mt-3" href="#services" type='submit'><i class="bi bi-arrow-right mx-3"></i>Servicios</a>
+                                    <a class="nav-link active mt-3" href="#services" type='submit'><i class="bi bi-scissors mx-3"></i>Servicios</a>
                                 </li>
                                 <li class="nav-item">
-                                    <a class="nav-link active mt-3" href="#prices"><i class="bi bi-arrow-right mx-3"></i>Precios</a>
+                                    <a class="nav-link active mt-3" href="#prices"><i class="bi bi-tag mx-3"></i>Precios</a>
                                 </li>
                                 <li class="nav-item">
-                                    <a class="nav-link active mt-3" href="#barbers"><i class="bi bi-arrow-right mx-3"></i>Nuestros Barberos</a>
+                                    <a class="nav-link active mt-3" href="#barbers"><i class="bi bi-people mx-3"></i>Nuestros Barberos</a>
                                 </li>
                                 <li class="nav-item">
-                                    <a class="nav-link active mt-3" href="#shop"><i class="bi bi-arrow-right mx-3"></i>¡¡Master Shop!!</a>
+                                    <a class="nav-link active mt-3" href="#shop"><i class="bi bi-cart mx-3"></i>¡¡Master Shop!!</a>
                                 </li>
                                 <li class="nav-item">
-                                    <a class="nav-link active mt-3" href="#turno"><i class="bi bi-arrow-right mx-3"></i>¡¡Reserva Aqui!!</a>
+                                    <a class="nav-link active mt-3" href="#turno"><i class="bi bi-calendar mx-3"></i>¿Por Que Reservar Con Nosotros?</a>
                                 </li>
                                 <li class="nav-item">
-                                    <a class="nav-link active mt-3" href="#galeria"><i class="bi bi-arrow-right mx-3"></i>Nuestra Galeria</a>
+                                    <a class="nav-link active mt-3" href="#galeria"><i class="bi bi-image mx-3"></i>Nuestra Galeria</a>
                                 </li>
                                 <li class="nav-item">
-                                    <a class="nav-link active mt-3" href="#ubicacion"><i class="bi bi-arrow-right mx-3"></i>Ubicacion</a>
+                                    <a class="nav-link active mt-3" href="#ubicacion"><i class="bi bi-geo-alt mx-3"></i>Ubicacion</a>
                                 </li>
                                 <li class="nav-item">
-                                    <a class="nav-link active mt-3" href="../Registro"><i class="bi bi-arrow-right mx-3"></i>Registrarme</a>
+                                    <a class="nav-link active mt-3" href="../Registro"><i class="bi bi-arrow-right mx-3"></i>Crear Mi Cuenta</a>
                                 </li>
 
                             </ul>
