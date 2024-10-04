fc5ffda4bbdec490a45aef0bb90d5cf374ec9b8d
FidelSmoke <146258944+FidelSmoke@users.noreply.github.com>
2024-10-04 11:06:42 -0500

    idexUser And NavbarUser

--- Mater Barber/Frontend/src/Components/NavbarUserIndex.jsx
+++ Mater Barber/Frontend/src/Components/NavbarUserIndex.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 
 
-export default function Navbaruserinde() {
+
+export default function NavbarUserIndex() {
     return (
         <div className='navbar shadow'>
             <div class="container-fluid">
@@ -10,24 +11,24 @@ export default function Navbaruserinde() {
 
 
 
-                    <button type="button" class="btn btn-dark border-light position-absolute mt-3 position-absolute top-0 end-0 translate-middle-x">
+                    <button type="button" class="btn btn-dark border-light position-absolute mt-3 position-absolute top-0 end-0 translate-middle-x" data-bs-toggle="offcanvas" data-bs-target="#offcanvasTop" aria-controls="offcanvasTop">
                         <i class="bi bi-alarm-fill"></i>
                         <span class="position-absolute top-0 start-100 translate-middle badge border border-light rounded-circle bg-danger p-2"><span class="visually-hidden">unread messages</span></span>
                     </button>
 
-                    <ul class="nav nav-underline mx-4 mt-3  position-absolute top-0 start-50 text-center translate-middle-x">
+                    <ul class="nav nav-underline mx-4 mt-3 position-absolute top-0 start-50 text-center translate-middle-x">
                         <li class="nav-item">
-                            <a class="nav-link mx-5 text-white" href="#">Link</a>
+                            <a class="nav-link mx-5 text-primary" href="#">Link</a>
                         </li>
 
                         <li class="nav-item">
-                            <a class="nav-link mx-5 text-white" href="#">Link</a>
+                            <a class="nav-link mx-5 text-primary" href="#">Link</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link mx-5 text-white" href="#">Link</a>
+                            <a class="nav-link mx-5 text-primary" href="#">Link</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link mx-5 text-white" href="#">Link</a>
+                            <a class="nav-link mx-5 text-primary" href="#">Link</a>
                         </li>
 
                     </ul>
@@ -40,7 +41,7 @@ export default function Navbaruserinde() {
                         </button>
                         <ul class="dropdown-menu bg-dark">
                             <li>
-                                <a class="dropdown-item bebas text-white" href="/PerfilAdmin">Perfil</a>
+                                <a class="dropdown-item bebas text-white " href="/PerfilAdmin">Perfil</a>
                             </li>
                             <li>
                                 <a class="dropdown-item bebas text-white" href="/ConfiguracionAdmin">Configuración</a>
@@ -52,6 +53,14 @@ export default function Navbaruserinde() {
                             </li>
                         </ul>
                     </div>
+                </div>
+            </div>
+
+            {/* Modal Para Notificaciones */}
+            <div class="offcanvas offcanvas-top bg-dark" tabindex="-1" id="offcanvasTop" aria-labelledby="offcanvasTopLabel">
+                <div class="offcanvas-header">
+                    <h5 class="offcanvas-title text-white" id="offcanvasTopLabel"><i class="bi bi-alarm-fill mx-4 text-warning"></i>Mis Reservas</h5>
+                    <button type="button" class="btn-close bg-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
                 </div>
             </div>
         </div>
--- Mater Barber/Frontend/src/pages/Usuario/IndexUser.jsx
+++ Mater Barber/Frontend/src/pages/Usuario/IndexUser.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import Navbaruserinde from '../../Components/Navbaruserinde'
+import NavbarUserIndex from '../../Components/NavbarUserIndex'
 
 export default function IndexUser() {
     return (
         <div>
-            <Navbaruserinde/>
-            <div className="img position-absolute top-50 start-50 translate-middle row h-100 col-1 col-sm-12 nave"><img src="/LOGO.png" alt="" className='' /></div>
-            <div className='container p-5 nave col col-sm-12' id='home'>
+            <NavbarUserIndex/>
+            <div className="img position-fixed top-50 start-50 translate-middle row  col-1 col-sm-12 nab p-5"><img src="/LOGO.png" alt="" className='' /></div>
+            <div className='container p-5 nave col col-sm-12 contenido' id='home'>
                 <h1 className='text-white text-center display-1 anton fw-bold'>MASTER BARBER VIP</h1>
                 <h2 className='text-warning text-center mt-5 anton fw-bold'>BOGOTA</h2>
                 <p className='text-white text-center mt-5 p-5'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laudantium animi, cum quas delectus nulla voluptate velit, corrupti beatae in rerum sint, minima doloribus fugit officia quae dolor doloremque aspernatur voluptas!</p>
