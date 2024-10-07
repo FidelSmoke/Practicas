e1f151857ab46336125e616c8012be787e123d87
FidelSmoke <146258944+FidelSmoke@users.noreply.github.com>
2024-10-07 16:53:42 -0500

    Menu Hamburgesa

--- Mater Barber/Frontend/src/Components/NavbarUserIndex.jsx
+++ Mater Barber/Frontend/src/Components/NavbarUserIndex.jsx
@@ -4,25 +4,19 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 
 export default function NavbarUserIndex() {
     return (
-        <div className='navbar shadow'>
-            <div class="container-fluid">
-                <div class="">
-                    {/* <small className='d-none d-sm-block fs-4 fw-bold text-white mt-1 mx-3'>BIENVENIDO</small> */}
-
-
-
-                    <button type="button" class="btn btn-dark border-light position-absolute mt-3 position-absolute top-0 end-0 translate-middle-x" data-bs-toggle="offcanvas" data-bs-target="#offcanvasTop" aria-controls="offcanvasTop">
-                        <i class="bi bi-alarm-fill"></i>
-                        <span class="position-absolute top-0 start-100 translate-middle badge border border-light rounded-circle bg-danger p-2"><span class="visually-hidden">unread messages</span></span>
-                    </button>
-
-                    <ul class="nav nav-underline mx-4 mt-3 position-absolute top-0 start-50 text-center translate-middle-x">
+        <div className='navbar navbar-expand-md'>
+            <div class="container-fluid justify-content-end">
+                <button class="navbar-toggler bg-secondary" type="button" data-bs-toggle="collapse" data-bs-target="#menu" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="menu">
+                    <ul class="navbar-nav nav-underline me-auto position-fixed mx-4 mt-3 position-absolute top-0 start-50 translate-middle-x ">
                         <li class="nav-item">
-                            <a class="nav-link mx-5 text-primary" href="#">Link</a>
+                            <a class="nav-link mx-5 text-primary " href="#homeuser">Inicio</a>
                         </li>
 
                         <li class="nav-item">
-                            <a class="nav-link mx-5 text-primary" href="#">Link</a>
+                            <a class="nav-link  mx-5 text-primary" href="#">Link</a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link mx-5 text-primary" href="#">Link</a>
@@ -32,11 +26,13 @@ export default function NavbarUserIndex() {
                         </li>
 
                     </ul>
-
-
+                    <button type="button" class="btn btn-dark border-light  mt-3 position-fixed top-0 end-0 translate-middle-x" data-bs-toggle="offcanvas" data-bs-target="#offcanvasTop" aria-controls="offcanvasTop">
+                        <i class="bi bi-alarm-fill"></i>
+                        <span class="position-absolute top-0 start-100 translate-middle badge border border-light rounded-circle bg-danger p-2"><span class="visually-hidden">unread messages</span></span>
+                    </button>
 
                     <div class="dropdown pe-4 me-4" >
-                        <button class=" btn dropdown text-white d-none d-sm-block mx-2" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        <button class=" btn dropdown text-white mx-2 " type="button" data-bs-toggle="dropdown" aria-expanded="false">
                             <i class="bi bi-person-circle fs-2"></i>
                         </button>
                         <ul class="dropdown-menu bg-dark">
@@ -52,17 +48,14 @@ export default function NavbarUserIndex() {
                                     </i> Cerrar sesión</a>
                             </li>
                         </ul>
+
                     </div>
                 </div>
+
             </div>
 
-            {/* Modal Para Notificaciones */}
-            <div class="offcanvas offcanvas-top bg-dark" tabindex="-1" id="offcanvasTop" aria-labelledby="offcanvasTopLabel">
-                <div class="offcanvas-header">
-                    <h5 class="text-white anton" id="offcanvasTopLabel"><i class="bi bi-alarm-fill mx-4 text-warning"></i>Mis Reservas</h5>
-                    <button type="button" class="btn-close bg-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-                </div>
-            </div>
         </div>
+
+
     )
 }
--- Mater Barber/Frontend/src/pages/Usuario/IndexUser.jsx
+++ Mater Barber/Frontend/src/pages/Usuario/IndexUser.jsx
@@ -5,7 +5,25 @@ export default function IndexUser() {
     return (
         <div>
             <NavbarUserIndex />
-            <div className="img position-fixed top-50 start-50 translate-middle row h-100 col-1 col-sm-12 nab p-5"><img src="/LOGO.png" alt="" className='' /></div>
+            <div className="img position-fixed top-50 start-50 translate-middle row h-100 col-1 col-sm-12  mt-5 p-5 m-2"><img src="/LOGO.png" alt="" className='' /></div>
+            <div className='container-fluid p-5 nab table-responsive col col-sm-12' id='homeuser'>
+                <h1 className='text-white text-center display-1 anton fw-bold'>MASTER BARBER VIP</h1>
+                <h2 className='text-warning text-center mt-5 anton fw-bold'>BOGOTA</h2>
+                <p className='text-white text-center mt-5 p-5'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laudantium animi, cum quas delectus nulla voluptate velit, corrupti beatae in rerum sint, minima doloribus fugit officia quae dolor doloremque aspernatur voluptas!</p>
+
+            </div >
+            <div className='container-fluid p-5 nab table-responsive col col-sm-12' id='home'>
+                <h1 className='text-white text-center display-1 anton fw-bold'>MASTER BARBER VIP</h1>
+                <h2 className='text-warning text-center mt-5 anton fw-bold'>BOGOTA</h2>
+                <p className='text-white text-center mt-5 p-5'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laudantium animi, cum quas delectus nulla voluptate velit, corrupti beatae in rerum sint, minima doloribus fugit officia quae dolor doloremque aspernatur voluptas!</p>
+
+            </div >
+            <div className='container-fluid p-5 nab table-responsive col col-sm-12' id='home'>
+                <h1 className='text-white text-center display-1 anton fw-bold'>MASTER BARBER VIP</h1>
+                <h2 className='text-warning text-center mt-5 anton fw-bold'>BOGOTA</h2>
+                <p className='text-white text-center mt-5 p-5'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laudantium animi, cum quas delectus nulla voluptate velit, corrupti beatae in rerum sint, minima doloribus fugit officia quae dolor doloremque aspernatur voluptas!</p>
+
+            </div >
             <div className='container-fluid p-5 nab table-responsive col col-sm-12' id='home'>
                 <h1 className='text-white text-center display-1 anton fw-bold'>MASTER BARBER VIP</h1>
                 <h2 className='text-warning text-center mt-5 anton fw-bold'>BOGOTA</h2>
@@ -13,6 +31,13 @@ export default function IndexUser() {
 
             </div >
 
+            <div className='container-fluid p-5 nab table-responsive col col-sm-12' id='home'>
+                <h1 className='text-white text-center display-1 anton fw-bold'>MASTER BARBER VIP</h1>
+                <h2 className='text-warning text-center mt-5 anton fw-bold'>BOGOTA</h2>
+                <p className='text-white text-center mt-5 p-5'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laudantium animi, cum quas delectus nulla voluptate velit, corrupti beatae in rerum sint, minima doloribus fugit officia quae dolor doloremque aspernatur voluptas!</p>
 
-        </div>)
+            </div >
+
+        </div>
+    )
 }
