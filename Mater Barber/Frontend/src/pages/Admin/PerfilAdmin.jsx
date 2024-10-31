7305a00582117741876103854cf6ae5a3bdb4e83
FidelSmoke <146258944+FidelSmoke@users.noreply.github.com>
2024-10-30 23:50:16 -0500

    Perfil Admin . Retroceder

--- Mater Barber/Frontend/src/pages/Admin/PerfilAdmin.jsx
+++ Mater Barber/Frontend/src/pages/Admin/PerfilAdmin.jsx
@@ -8,12 +8,13 @@ export default function Login() {
     <div className="">
       <div className="min-vh-100 align-content-center mx-5 justify-content-end">
         <div className='container p-sm-5  border border-2 shadow border-white rounded-4 '>
+        <a href="/InicioAdmin"><i className="bi bi-arrow-left-circle-fill text-white fs-2"></i></a>
           <div className="row justify-content-center align-items-center">
-            <div className="col col-lg-6 bi-text-lg-center mt-5">
+            <div className="col col-lg-6 bi-text-lg-center ">
               <i className="bi bi-person-circle icono mt-5">
               </i>
             </div>
-            <div className="col-12 col-lg-6 mt-5 container">
+            <div className="col-12 col-lg-6 container">
               <h1 className='text-warning text-center anton mb-4'>¡Perfil!</h1>
 
               <form className='row g-1 mb-5'>
@@ -23,7 +24,7 @@ export default function Login() {
                     <input type="text" readonly className="form-control-plaintext text-white antonparabackend " id="staticEmail" value="SAYAAAA" />
                   </div>
                 </div>
-                <div className="form-floating mb-3    ">
+                <div className="form-floating mb-3 mx-2">
                   <input type="text" className="form-control bg-dark text-white mt-2" id="floatingInput" placeholder="name@example.com" />
                   <label for="floatingInput" className='text-dark' >Email address</label>
                 </div>
